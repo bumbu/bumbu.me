dc339c35f6d2f8fae671b38dc7a910324a53053f
bumbu <bmbalex@gmail.com>
2013-04-14 02:35:58 +0300

    add coffee and less to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,68 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
+  // Project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      build: {
-        src: 'src/js/application.js',
-        dest: 'app/wp-content/themes/bumbu/js/application.min.js'
+    coffee: {
+      production: {
+        files: [{
+          expand: true,
+          cwd: 'src/scripts',
+          src: '*.coffee',
+          dest: 'app/wp-content/themes/bumbu/js',
+          ext: '.js'
+        }]
+      }
+    },
+    less: {
+      development: {
+        options: {
+          paths: ["src/styles"]
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/styles',
+          src: '*.build.less',
+          dest: 'app/wp-content/themes/bumbu/css',
+          ext: '.css'
+        }]
+      },
+      production: {
+        options: {
+          paths: ["src/styles"],
+          yuicompress: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/styles',
+          src: '*.build.less',
+          dest: 'app/wp-content/themes/bumbu/css',
+          ext: '.css'
+        }]
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  // Tasks definitions
+  grunt.registerTask('server', [
+    'coffee'
+  ]);
+
+  grunt.registerTask('test', [
+  ]);
+
+  grunt.registerTask('build', [
+    'coffee',
+    'less'
+  ]);
+
+  grunt.registerTask('default', [
+    // 'jshint',
+    'test',
+    'build'
+  ]);
 
 };
