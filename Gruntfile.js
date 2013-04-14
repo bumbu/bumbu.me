6797eae735f0d758c3559cfb87ea481bb1fca3e6
bumbu <bmbalex@gmail.com>
2013-04-14 04:54:50 +0300

    add livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,43 @@
+'use strict';
+
 module.exports = function(grunt) {
 
   // Project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      coffee: {
+        files: ['src/scripts/*.coffee'],
+        tasks: ['coffee:production']
+      },
+      less: {
+        files: ['src/styles/*.build.less'],
+        tasks: ['less:development']
+      },
+      livereload: {
+        files: [
+          'app/wp-content/themes/bumbu/{,*/}*.php',
+          'app/wp-content/themes/bumbu/{,*/}*.css',
+          'app/wp-content/themes/bumbu/{,*/}*.js',
+          'app/wp-content/themes/bumbu/{,*/}*.{png,jpg,jpeg,webp}'
+        ],
+        tasks: ['livereload']
+      }
+    },
     coffee: {
       production: {
+        files: [{
+          expand: true,
+          cwd: 'src/scripts',
+          src: '*.coffee',
+          dest: 'app/wp-content/themes/bumbu/js',
+          ext: '.js'
+        }]
+      },
+      development: {
+        options: {
+          sourceMap: true
+        },
         files: [{
           expand: true,
           cwd: 'src/scripts',
@@ -43,20 +76,30 @@ module.exports = function(grunt) {
     }
   });
 
+  // https://github.com/yeoman/yeoman/issues/250#issuecomment-8024212
+  // https://github.com/gruntjs/grunt-contrib-connect
+  // https://github.com/romainberger/yeoman-wordpress/blob/master/app/templates/Gruntfile.js
+  // https://github.com/gruntjs/grunt-contrib-livereload/pull/32
+
+  grunt.loadNpmTasks('grunt-contrib-livereload');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Tasks definitions
   grunt.registerTask('server', [
-    'coffee'
+    'livereload-start',
+    'coffee:development',
+    'less:development',
+    'watch'
   ]);
 
   grunt.registerTask('test', [
   ]);
 
   grunt.registerTask('build', [
-    'coffee',
-    'less'
+    'coffee:production',
+    'less:production'
   ]);
 
   grunt.registerTask('default', [
