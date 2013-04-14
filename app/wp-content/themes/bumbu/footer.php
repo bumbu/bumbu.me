d35e146bdaf1887837812b17a4b61c9ac416a80d
bumbu <bmbalex@gmail.com>
2013-04-14 19:47:55 +0300

    make panels to stay right

--- app/wp-content/themes/bumbu/footer.php
+++ app/wp-content/themes/bumbu/footer.php
@@ -10,15 +10,45 @@
 
 	</div><!-- #main -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'bumbu_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'bumbu' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'bumbu' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'bumbu' ), 'bumbu', '<a href="http://bumbu.ru" rel="designer">bumbu</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
+<div class="panels">
+	<div class="panel-main open_">
+		<a href="#" class="logo"><i class="icon icon-bumbu-logo"></i><div class="logo-image"><img src="<?php bloginfo( 'template_url' ); ?>/img/logo-bumbu-text.png" alt="bumbu logo" width="116" height="24"></div></a>
+		<ul class="menu">
+			<li class="active"><a class="icon icon-blog" href="#">Blog</a></li>
+			<li><a class="icon icon-about" href="#">About</a></li>
+			<li><a class="icon icon-catalog" href="#">Catalog</a></li>
+			<li><a class="icon icon-experiments" href="#">Experiments</a></li>
+		</ul>
+	</div>
+	<div class="panel-first">
+		<div class="item searchbar"></div>
+		<div class="item active">
+			<div class="title">Awesome post title</div>
+			<div class="content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</div>
+		</div>
+		<div class="item">
+			<div class="title">Awesome post title</div>
+			<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
+		</div>
+		<div class="item">
+			<div class="title">Awesome post title</div>
+			<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
+		</div>
+		<div class="item">
+			<div class="title">Awesome post title</div>
+			<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
+		</div>
+		<div class="item">
+			<div class="title">Awesome post title</div>
+			<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
+		</div>
+		<div class="item">
+			<div class="title">Awesome post title</div>
+			<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
+		</div>
+	</div>
+</div>
 
 <?php wp_footer(); ?>
 
--- app/wp-content/themes/bumbu/header.php
+++ app/wp-content/themes/bumbu/header.php
@@ -24,45 +24,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<div class="panels">
-		<div class="panel-main open_">
-			<a href="#" class="logo"><i class="icon icon-bumbu-logo"></i><div class="logo-image"><img src="<?php bloginfo( 'template_url' ); ?>/img/logo-bumbu-text.png" alt="bumbu logo" width="116" height="24"></div></a>
-			<ul class="menu">
-				<li class="active"><a class="icon icon-blog" href="#">Blog</a></li>
-				<li><a class="icon icon-about" href="#">About</a></li>
-				<li><a class="icon icon-catalog" href="#">Catalog</a></li>
-				<li><a class="icon icon-experiments" href="#">Experiments</a></li>
-			</ul>
-		</div>
-		<div class="panel-first">
-			<div class="item searchbar"></div>
-			<div class="item active">
-				<div class="title">Awesome post title</div>
-				<div class="content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</div>
-			</div>
-			<div class="item">
-				<div class="title">Awesome post title</div>
-				<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
-			</div>
-			<div class="item">
-				<div class="title">Awesome post title</div>
-				<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
-			</div>
-			<div class="item">
-				<div class="title">Awesome post title</div>
-				<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
-			</div>
-			<div class="item">
-				<div class="title">Awesome post title</div>
-				<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
-			</div>
-			<div class="item">
-				<div class="title">Awesome post title</div>
-				<div class="content">Lorem dolor sit amet, conuer adipiscing elit. Aenean commodo dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </div>
-			</div>
-		</div>
-	</div>
 <div id="page" class="body-container hfeed site">
 	<?php do_action( 'before' ); ?>
-
 	<div id="main" class="site-main">
