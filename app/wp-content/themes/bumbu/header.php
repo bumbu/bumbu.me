a40cff42265229a99f22c2bc8cd351cafafe3b2d
bumbu <bmbalex@gmail.com>
2013-04-15 05:43:37 +0300

    add favicon

--- app/wp-content/themes/bumbu/header.php
+++ app/wp-content/themes/bumbu/header.php
@@ -14,6 +14,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" href="<?php bloginfo( 'template_url' ); ?>/favicon.ico" type="image/x-icon">
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
