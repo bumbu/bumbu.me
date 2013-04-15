6fffd3a3f34a82cf15d9f229497bf3eaae3f1755
bumbu <bmbalex@gmail.com>
2013-04-15 04:03:08 +0300

    simplify pages even more

--- app/wp-content/themes/bumbu/page.php
+++ app/wp-content/themes/bumbu/page.php
@@ -9,24 +9,16 @@
  *
  * @package bumbu
  */
-
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-
+		<?php if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template();
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
+				<?php get_template_part( 'content' ); ?>
+			<?php endwhile; ?>
+		<?php else : ?>
+			<?php get_template_part( 'no-results', 'index' ); ?>
+		<?php endif; ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
-
 <?php get_footer(); ?>
