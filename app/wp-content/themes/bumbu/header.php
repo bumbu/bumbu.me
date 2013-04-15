ab8330a46a1d123163d975ce4ceda7940ca8320f
bumbu <bmbalex@gmail.com>
2013-04-15 05:37:45 +0300

    add google tracking code

--- app/wp-content/themes/bumbu/header.php
+++ app/wp-content/themes/bumbu/header.php
@@ -21,6 +21,21 @@
 <?php wp_head(); ?>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/css/font.css" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/css/application.css" />
+
+<script type="text/javascript">
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-34516109-2']);
+	_gaq.push(['_setDomainName', 'bumbu.ru']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
+</script>
 </head>
 
 <body <?php body_class(); ?>>
